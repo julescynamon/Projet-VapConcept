2c7a37128ad875258d46d55673f077bdf7c7f222
jules cynamon <julescweb@gmail.com>
2023-05-31 08:29:27 +0200

    logique pour importer la facture ok

--- js/facture.js
+++ js/facture.js
@@ -1,3 +1,7 @@
+const Papa = require("papaparse");
+const path = require("path");
+const fs = require("fs");
+
 // Sélectionnez le formulaire et ajoutez un écouteur d'événements 'submit'
 const form = document.getElementById("csv-form");
 form.addEventListener("submit", handleFormSubmit);
@@ -5,7 +9,7 @@ form.addEventListener("submit", handleFormSubmit);
 function handleFormSubmit(event) {
   event.preventDefault(); // Empêche le rechargement de la page
 
-  // Récupérez le fichier .xlsx sélectionné
+  // Récupérez le fichier .csv sélectionné
   const fileInput = event.target.elements.file;
   const file = fileInput.files[0];
 
@@ -16,34 +20,44 @@ function handleFormSubmit(event) {
     return;
   }
 
-  // Lisez le contenu du fichier .xlsx
+  // Lisez le contenu du fichier .csv
   const reader = new FileReader();
   reader.onload = function (e) {
     const csvContent = e.target.result;
 
-    // Convertissez le .xlsx en JSON
+    // Convertissez le .csv en JSON
     const json = csvToJson(csvContent);
     console.log(json);
 
     try {
-      // Supprimez l'ancien fichier bdd.json s'il existe
+      // Supprimez l'ancien fichier facture.json s'il existe
       const facturePath = path.join(__dirname, "facture.json");
       if (fs.existsSync(facturePath)) {
         fs.unlinkSync(facturePath);
       }
 
-      // Créez un nouveau fichier bdd.json avec le contenu JSON
+      // Créez un nouveau fichier facture.json avec le contenu JSON
       fs.writeFileSync(facturePath, JSON.stringify(json));
 
-      // Affichez un message à l'utilisateur pour indiquer que la base de données a bien été importée
+      // Affichez un message à l'utilisateur pour indiquer que la facture a bien été importée
       document.getElementById("facture-success").textContent =
         "La base de données a bien été importée";
     } catch (error) {
       // Affichez un message d'erreur à l'utilisateur
       document.getElementById("facture-error").textContent =
-        "Une erreur s'est produite lors de l'importation de la base de données";
+        "Une erreur s'est produite lors de l'importation de la facture";
       console.error(error);
     }
   };
-  reader.readAsArrayBuffer(file);
+  reader.readAsText(file);
+}
+
+function csvToJson(csvContent) {
+  // Utilisez PapaParse pour lire le contenu du fichier .csv
+  const parsedData = Papa.parse(csvContent, { header: true });
+
+  // Récupérez les données JSON
+  const json = parsedData.data;
+
+  return json;
 }
