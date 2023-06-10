4c756901e5c41c5ba958e044883f9ddfefabd6ee
jules cynamon <julescweb@gmail.com>
2023-06-10 18:12:04 +0200

    on avance

--- js/factureAutre.js
+++ js/factureAutre.js
@@ -1,84 +1,32 @@
-const path = require("path");
-const fs = require("fs");
-const pdfjsLib = require("pdfjs-dist");
-
-pdfjsLib.GlobalWorkerOptions.workerSrc =
-  "./node_modules/pdfjs-dist/build/pdf.worker.js";
+import fs from "fs";
+import PDFParser from "pdf2json";
 
 // Fonction pour convertir un fichier PDF en JSON
-async function convertPDFToJSON(pdfFile) {
-  const pdfData = await readFileAsArrayBuffer(pdfFile);
+const convertPDFToJSON = (file) => {
+  return new Promise((resolve, reject) => {
+    const pdfParser = new PDFParser();
 
-  // Fonction pour lire le fichier PDF sous forme de ArrayBuffer
-  function readFileAsArrayBuffer(file) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
+    pdfParser.on("pdfParser_dataError", (errData) =>
+      reject(errData.parserError)
+    );
+    pdfParser.on("pdfParser_dataReady", (pdfData) => {
+      const textContent = pdfParser.getRawTextContent();
+      const lines = textContent.split("\n");
 
-      reader.onload = () => {
-        resolve(reader.result);
+      const jsonData = {
+        numero: lines[0],
+        date: lines[1],
+        client: lines[2],
+        totalHT: lines[3],
+        totalTTC: lines[4],
       };
 
-      reader.onerror = reject;
-
-      reader.readAsArrayBuffer(file);
+      resolve(jsonData);
     });
-  }
 
-  // Fonction pour extraire les données du PDF
-  async function extractDataFromPDF(pdfData) {
-    const pdf = await pdfjsLib.getDocument({ data: pdfData }).promise;
-    const totalPages = pdf.numPages;
-    const extractedData = [];
-
-    // Parcourir chaque page du PDF
-    for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++) {
-      const page = await pdf.getPage(pageNumber);
-      const pageText = await page.getTextContent();
-      const pageItems = pageText.items;
-
-      console.log(pageText);
-
-      // Analyser les éléments de texte pour extraire les données
-      // Utilisez des expressions régulières ou d'autres méthodes pour extraire les données souhaitées
-      // Dans cet exemple, nous supposons un modèle de facture avec des colonnes prédéfinies
-      const rows = [];
-      for (const item of pageItems) {
-        const { str } = item;
-        console.log(str);
-        // Exemple de modèle de facture avec des colonnes prédéfinies
-        const columns = str.split("\n");
-
-        if (columns.length >= 5) {
-          const reference = columns["Référence"];
-          const designation = columns["Désignation"];
-          const quantity = columns["Quantité"];
-          const unitPrice = columns["PU HT"];
-          const totalPrice = columns["Montant HT"];
-
-          // Créer un objet JSON pour chaque ligne de la facture
-          const invoiceRow = {
-            reference: reference.trim(),
-            designation: designation.trim(),
-            quantity: quantity.trim(),
-            unitPrice: unitPrice.trim(),
-            totalPrice: totalPrice.trim(),
-          };
-
-          rows.push(invoiceRow);
-        }
-      }
-
-      extractedData.push(...rows);
-    }
-
-    return extractedData;
-  }
-
-  // Appel de la fonction pour extraire les données du PDF
-  const extractedData = await extractDataFromPDF(pdfData);
-
-  return extractedData;
-}
+    pdfParser.loadPDF(file);
+  });
+};
 
 // Récupération du formulaire
 const pdfForm = document.getElementById("pdf-form");
