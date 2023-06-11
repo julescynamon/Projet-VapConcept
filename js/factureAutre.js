52126de75a24fdede0cc4ec9aa778b75ded9a605
jules cynamon <julescweb@gmail.com>
2023-06-11 08:52:11 +0200

    on avance sur la conversion du pdf

--- js/factureAutre.js
+++ js/factureAutre.js
@@ -1,37 +1,5 @@
-import fs from "fs";
-import PDFParser from "pdf2json";
-
-// Fonction pour convertir un fichier PDF en JSON
-const convertPDFToJSON = (file) => {
-  return new Promise((resolve, reject) => {
-    const pdfParser = new PDFParser();
-
-    pdfParser.on("pdfParser_dataError", (errData) =>
-      reject(errData.parserError)
-    );
-    pdfParser.on("pdfParser_dataReady", (pdfData) => {
-      const textContent = pdfParser.getRawTextContent();
-      const lines = textContent.split("\n");
-
-      const jsonData = {
-        numero: lines[0],
-        date: lines[1],
-        client: lines[2],
-        totalHT: lines[3],
-        totalTTC: lines[4],
-      };
-
-      resolve(jsonData);
-    });
-
-    pdfParser.loadPDF(file);
-  });
-};
-
-// Récupération du formulaire
 const pdfForm = document.getElementById("pdf-form");
 
-// Écouteur d'événement pour la soumission du formulaire
 pdfForm.addEventListener("submit", async (event) => {
   event.preventDefault(); // Empêcher le comportement de soumission par défaut
 
@@ -42,6 +10,33 @@ pdfForm.addEventListener("submit", async (event) => {
     alert("Veuillez sélectionner un fichier PDF.");
     return;
   }
+
+  const PDFParser = await import("pdf2json").then((module) => module.default);
+  const convertPDFToJSON = (file) => {
+    return new Promise((resolve, reject) => {
+      const pdfParser = new PDFParser();
+
+      pdfParser.on("pdfParser_dataError", (errData) =>
+        reject(errData.parserError)
+      );
+      pdfParser.on("pdfParser_dataReady", (pdfData) => {
+        const textContent = pdfParser.getRawTextContent();
+        const lines = textContent.split("\n");
+
+        const jsonData = {
+          numero: lines[0],
+          date: lines[1],
+          client: lines[2],
+          totalHT: lines[3],
+          totalTTC: lines[4],
+        };
+
+        resolve(jsonData);
+      });
+
+      pdfParser.loadPDF(file);
+    });
+  };
 
   const jsonData = await convertPDFToJSON(file);
 
